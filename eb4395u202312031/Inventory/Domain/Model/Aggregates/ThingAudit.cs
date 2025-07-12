b290bd94d71ef48a6c1ea34d9e9db7f0ecbbffc7
Alison Arrieta <alisonarrieta06@gmail.com>
2025-07-12 16:51:46 -0500

    commit:  add thingAudit file.

---
+++ eb4395u202312031/Inventory/Domain/Model/Aggregates/ThingAudit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace eb4395u202312031.Inventory.Domain.Model.Aggregates;
+
+public partial class Thing : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
